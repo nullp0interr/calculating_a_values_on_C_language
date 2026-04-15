7e1c3f8ae4b8c4c17f3e46e11040bc21d4a5fc0d
Mikhail Ivanov <gm.mishanya@gmail.com>
2026-04-15 13:05:32 +0200

    files upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int multiply ( int a, int b );
+
+int main(void) {
+    int a;
+    int b;
+
+    printf("Input two numbers to be multiplied: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    return 0;
+}
+
+
+//
