8bcf4d72ab6cdd88a4ee452fe30d9fd903aed5cd
Mikhail Ivanov <gm.mishanya@gmail.com>
2026-04-15 13:13:26 +0200

    method of multiplying has implemented and successfull work

--- main.c
+++ main.c
@@ -7,10 +7,17 @@ int main(void) {
     int b;
 
     printf("Input two numbers to be multiplied: ");
+
     scanf("%d", &a);
     scanf("%d", &b);
+
+    printf("Result of multiply operation (%d * %d) = %d)",
+        a, b, multiply(a, b));
+
+    getchar();
     return 0;
 }
 
-
-//
+int multiply(int a, int b) {
+    return a * b;
+}
